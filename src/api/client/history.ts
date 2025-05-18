16e572d3c2fab1d492ef1b2cd3a8f5a43d1f6092
Jiwon Choi <won0805@ewhain.net>
2025-05-18 18:59:35 +0900

    feat: 대시보드 및 상세 데이터

--- src/api/client/history.ts
+++ src/api/client/history.ts
@@ -6,10 +6,31 @@ export type DashboardItem = {
   last_modified: string;
 };
 
+export type DashboardDetail = {
+  id: number;
+  name: string;
+  date: string;
+  details: {
+    id: number;
+    epoch: number;
+    batch_size: number;
+    learning_rate: number;
+    loss: number;
+    accuracy: number;
+    date: string;
+  }[];
+};
+
 export const historyApi = {
-  // 코드 생성하기
+  // 대시보드 조회
   getDashboard: async () => {
     const response = await privateApi.get<DashboardItem[]>(`/dashboard`);
+    return response;
+  },
+
+  // 대시보드 상세 조회
+  getDashboardDetail: async (id: number) => {
+    const response = await privateApi.get<DashboardDetail>(`/dashboard/${id}`);
     return response;
   },
 };
